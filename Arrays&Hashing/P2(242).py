31476100ee35bdf54baa865ac4ecb2ebea7c647d
Jacob <58263528+mykasero@users.noreply.github.com>
2023-05-10 22:32:29 +0200

    Add files via upload

---
+++ Arrays&Hashing/P2(242).py
@@ -0,0 +1,28 @@
+s = "aacc"
+t = "ccac"
+'''
+#original idea (passed 33/38)
+
+count=0
+
+for i in s:
+    if i in t:
+        count+=1
+    
+if count == len(s):
+    print(True)
+else:
+    print(False)
+'''
+# fully passed idea
+
+if len(s) != len(t):
+    print(False)
+for i in set(s):
+    # Compare s.count(l) and t.count(l) for every index i from 0 to 26...
+    # If they are different, return false...
+    print("s count: ",s.count(i),'| t count: ',t.count(i))
+    # if s.count(i) != t.count(i):
+    #     print(False)
+print(True)       
+
